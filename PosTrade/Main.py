2e5b6ce3eea3a695e3b7a871bb080ef6fe99cdb9
KG <gowthamkumar2012@gmail.com>
2019-03-19 02:10:21 -0400

    design objectives thought off
    Signed-off-by: KG <gowthamkumar2012@gmail.com>

---
+++ PosTrade/Main.py
@@ -0,0 +1,9 @@
+# intializes feed adpaters and directs data to different processes
+
+## feed adapters can be an api, from a text file or a csv file
+
+## each real tile feed is to be processed on a distributed system and run time calculations and
+# key strategy developments(model improvements) based on the old data and the current feed.
+
+## decide on the distributed architecture and data bases to be used and
+## properly and precisely design the model outputs
